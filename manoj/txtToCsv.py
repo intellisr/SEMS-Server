52814bb80e25bb7fca7da5f33fe1c04705741d4d
intellisr <intellisr@gmail.com>
2020-05-09 22:16:28 +0530

    second attempt

--- manoj/txtToCsv.py
+++ manoj/txtToCsv.py
@@ -25,17 +25,3 @@ values = dataset.values
 dataset['sub_metering_4'] = (values[:,0] * 1000 / 60) - (values[:,4] + values[:,5] + values[:,6])
 # save updated dataset
 dataset.to_csv('household_power_consumption.csv')
-
-#categraizes data in to day
-# resample minute data to total for each day
-from pandas import read_csv
-# load the new file
-dataset = read_csv('household_power_consumption.csv', header=0, infer_datetime_format=True, parse_dates=['datetime'], index_col=['datetime'])
-# resample data to daily
-daily_groups = dataset.resample('D')
-daily_data = daily_groups.sum()
-# summarize
-print(daily_data.shape)
-print(daily_data.head())
-# save
-daily_data.to_csv('household_power_consumption_days.csv')
